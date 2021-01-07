69f3b46274b45beee99e9792e82b07520cf00434
Boris Ermakov-Spektor <boris.ermakov-spektor@ukg.com>
2021-01-07 12:34:10 -0500

    Make work with stomp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import SockJsClient from 'react-stomp';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  sendMessage = (msg) => {
+    this.clientRef.sendMessage('/topics/all', msg);
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas class="whiteboard" ></canvas>
+
+        <div class="colors">
+          <div class="color black"></div>
+          <div class="color red"></div>
+          <div class="color green"></div>
+          <div class="color blue"></div>
+          <div class="color yellow"></div>
+        </div>
+        <SockJsClient url='http://localhost:8080/ws' topics={['/topics/all']}
+          onMessage={(msg) => { console.log(msg); }}
+          ref={(client) => { this.clientRef = client }} />
+      </div>
+    );
+  }
 }
 
 export default App;
